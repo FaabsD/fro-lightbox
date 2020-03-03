6342fa6e8f36de5447ce8d2ec8fc7243ab412748
Fabian Hendriks <19959@ma-web.nl>
2020-03-03 11:34:58 +0100

    nieuwe fuctie en for loop gemaakt

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,8 +1,18 @@
 const alleInformatie = document.querySelectorAll('.info');
-const alleThumbnails = document.querySelectorAll('thumbnail');
+const alleThumbnails = document.querySelectorAll('.thumbnail');
 
 let inhoud = [];
 for(let i=0; i<alleInformatie.length; i++){
     inhoud.push(alleInformatie[i]);
     alleInformatie[i].remove();
+}
+
+function makeModal(num) {
+    console.log(inhoud[num]);
+}
+
+for (let i=0; i<alleThumbnails.length; i++) {
+    alleThumbnails[i].addEventListener('click', function() {
+        makeModal(i)
+    })
 }
